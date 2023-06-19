b65726e402a62f0932da372310fd89be1a90028a
PRATIK KUMAR BISWAL <117885087+PratikMiku@users.noreply.github.com>
2023-06-19 21:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function compareWithoutArticle(a, b) {
+  const ignoreArticles = /^(a|an|the)\s+/i;
+  const cleanA = a.replace(ignoreArticles, '');
+  const cleanB = b.replace(ignoreArticles, '');
+  return cleanA.localeCompare(cleanB);
+}
 
+touristSpots.sort(compareWithoutArticle);
+console.log(touristSpots);
